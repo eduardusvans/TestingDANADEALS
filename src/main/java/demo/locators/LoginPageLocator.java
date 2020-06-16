16b4d72a2542fa3727a62e4449026edaee14c5ed
orianisihaloho <orianisihaloho@hotmail.com>
2020-06-16 12:41:32 +0700

    Create Login Page Locator

--- src/main/java/demo/locators/LoginPageLocator.java
+++ src/main/java/demo/locators/LoginPageLocator.java
@@ -1,4 +1,17 @@
 package demo.locators;
 
+import org.openqa.selenium.By;
+
 public interface LoginPageLocator {
+    By APP_NAME_LABEL = By.id("");
+    By EDIT_TEXT_PHONE = By.id("et_phone");
+    By EDIT_TEXT_PASSWORD = By.id("et_password");
+    By BUTTON_LOGIN = By.id("btn_login");
+
+    By LINK_FORGOT = By.id("tv_forgot");
+    By LINK_REGISTER = By.id("tv_register_here");
+
+    By TXT_COUNTRY_CODE = By.id("");
+    By TXT_NOT_REGISTER = By.id("tv_not_yet_registered");
+
 }
--- src/main/java/demo/pages/LoginPage.java
+++ src/main/java/demo/pages/LoginPage.java
@@ -12,8 +12,19 @@ import static demo.locators.LoginPageLocator.*;
 public class LoginPage {
     public boolean isOnPage() {
         WebDriverWait wait = new WebDriverWait(androidDriver, 15);
-        WebElement passwordInput = wait
-                .until(ExpectedConditions.presenceOfElementLocated(By.id("password_textbox")));
-        return passwordInput.isDisplayed();
+        WebElement AppNameLabel = wait
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("")));
+        return AppNameLabel.isDisplayed();
     }
+
+    public void inputPhoneNumber(String phoneNumber){
+        androidDriver.findElement(EDIT_TEXT_PHONE).sendKeys(phoneNumber);
+    }
+
+    public void inputPassword(String password){
+        androidDriver.findElement(EDIT_TEXT_PASSWORD).sendKeys(password);
+    }
+
+    public void clickLogin(){ androidDriver.findElement(BUTTON_LOGIN).click(); }
+
 }
