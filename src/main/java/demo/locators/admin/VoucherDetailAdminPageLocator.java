6ff7ce363f160a6a881dbb6c15f094d62e434808
orianisihaloho <orianisihaloho@hotmail.com>
2020-06-17 14:25:57 +0700

    Update VoucherDetailAdminPagelocator, VoucherDetailAdminPage, and VoucherDetailPayerPage

--- src/main/java/demo/locators/admin/VoucherDetailAdminPageLocator.java
+++ src/main/java/demo/locators/admin/VoucherDetailAdminPageLocator.java
@@ -1,4 +1,20 @@
 package demo.locators.admin;
 
+import org.openqa.selenium.By;
+
 public interface VoucherDetailAdminPageLocator {
+
+    By TXT_STATUS           = By.id("tv_voucher_status");
+    By TXT_VOUCHER_NAME     = By.id("tv_voucher_name_admin");
+    By TXT_MERCHANT_ID      = By.id("tv_merchant_name_admin");
+    By TXT_DISCOUNT         = By.id("tv_discount_admin");
+    By TXT_MAX_DISCOUNT     = By.id("tv_max_discount_admin");
+    By TXT_VOUCHER_PRICE    = By.id("tv_voucher_price_admin");
+    By TXT_EXPIRED          = By.id("tv_expired_admin");
+    By EDIT_TEXT_QUOTA      = By.id("quota_number");
+
+    By BUTTON_INCREASE = By.id("increase");
+    By BUTTON_DECREASE = By.id("decrease");
+    By BUTTON_SAVE     = By.id("btn_save");
+
 }
--- src/main/java/demo/pages/admin/VoucherDetailAdminPage.java
+++ src/main/java/demo/pages/admin/VoucherDetailAdminPage.java
@@ -1,5 +1,76 @@
 package demo.pages.admin;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+import static demo.driver.AndroidDriverInstance.androidDriver;
+import static demo.locators.LoginPageLocator.*;
 import static demo.locators.admin.VoucherDetailAdminPageLocator.*;
 
 public class VoucherDetailAdminPage {
+    public boolean isOnPage() {
+        WebDriverWait wait = new WebDriverWait(androidDriver, 15);
+        WebElement savebutton = wait
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("btn_save")));
+        return savebutton.isDisplayed();
+    }
+    public boolean seeStatusVoucher() {
+        WebDriverWait wait = new WebDriverWait(androidDriver, 15);
+        WebElement statusLabel = wait
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("tv_voucher_status")));
+        return statusLabel.isDisplayed();
+    }
+
+    public boolean seeVoucherName() {
+        WebDriverWait wait = new WebDriverWait(androidDriver, 10);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(By.id("tv_voucher_name_admin"))).isDisplayed();
+    }
+    public boolean seeMerchantID() {
+        WebDriverWait wait = new WebDriverWait(androidDriver, 10);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(By.id("tv_merchant_name_admin"))).isDisplayed();
+    }
+    public boolean seeDiscount() {
+        WebDriverWait wait = new WebDriverWait(androidDriver, 10);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(By.id("tv_discount_admin"))).isDisplayed();
+    }
+    public boolean seeMaxDiscount() {
+        WebDriverWait wait = new WebDriverWait(androidDriver, 10);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(By.id("tv_max_discount_admin"))).isDisplayed();
+    }
+    public boolean seeVoucherPrice() {
+        WebDriverWait wait = new WebDriverWait(androidDriver, 10);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(By.id("tv_voucher_price_admin"))).isDisplayed();
+    }
+    public boolean seeExpired() {
+        WebDriverWait wait = new WebDriverWait(androidDriver, 10);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(By.id("tv_expired_admin"))).isDisplayed();
+    }
+
+    public void selectStatus(String statusName) {
+        androidDriver.findElement(TXT_STATUS).click();
+
+        String xpath = "";
+        List<AndroidElement> statuss = androidDriver.findElements(By.xpath(xpath));
+
+        for (AndroidElement status : statuss) {
+            if (status.getText().equalsIgnoreCase(statusName)) {
+                status.click();
+                break;
+            }
+        }
+    }
+
+    public void inputQuota(String quota){
+        androidDriver.findElement(EDIT_TEXT_QUOTA).sendKeys(quota);
+    }
+
+    public void clickIncrease(){ androidDriver.findElement(BUTTON_INCREASE).click(); }
+    public void clickDecrease(){ androidDriver.findElement(BUTTON_DECREASE).click(); }
+    public void clickSave(){ androidDriver.findElement(BUTTON_SAVE).click(); }
+
+
 }
