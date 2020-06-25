4833b1153939bf3ef604cb9647ae2930c18e5013
eduardusvans <eduardus.arderito@dana.id>
2020-06-25 14:41:10 +0700

    Update profile

--- src/main/java/demo/pages/payer/profile/ChangePasswordPage.java
+++ src/main/java/demo/pages/payer/profile/ChangePasswordPage.java
@@ -1,9 +1,14 @@
 package demo.pages.payer.profile;
 
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+
 import static demo.locators.payer.profile.ChangePasswordPageLocator.*;
 import static demo.driver.AndroidDriverInstance.androidDriver;
-import static demo.utils.ActionUtils.tapElement;
-import static demo.utils.ActionUtils.waitElement;
+import static demo.utils.ActionUtils.*;
 
 public class ChangePasswordPage {
     public boolean isOnPageChangePassword() {
@@ -26,5 +31,35 @@ public class ChangePasswordPage {
     }
     public boolean seeWarningText(){
         return waitElement(warningText,15).isDisplayed();
+    }
+
+    public static void scrollDown() {
+        AndroidElement screen = androidDriver
+                .findElement(By.id("action_bar_root"));
+        Point center =  screen.getCenter();
+        int startX = 20;
+        int startY = (int) (center.getY() * 1.5);
+        int endX = 20;
+        int endY = (int) (center.getY() * 0.5);
+        @SuppressWarnings("rawtypes")
+        TouchAction scroll = new TouchAction(androidDriver);
+        scroll.press(PointOption.point(startX, startY))
+                .moveTo(PointOption.point(endX, endY)).perform();
+    }
+
+    public static void inputAndScroll(By targetElement, String input) {
+        boolean isFound = false;
+        int counter = 0;
+
+        do {
+            try {
+                inputElement(targetElement, input);
+                isFound = true;
+            } catch (Exception e) {
+                scrollDown();
+                counter++;
+            }
+
+        } while (!isFound && counter < 5);
     }
 }
