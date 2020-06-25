127d3323a53c71bbabbbe84f46f36849f73711d4
eduardusvans <eduardus.arderito@dana.id>
2020-06-25 15:06:26 +0700

    Update profile

--- src/main/java/demo/pages/payer/profile/ChangePasswordPage.java
+++ src/main/java/demo/pages/payer/profile/ChangePasswordPage.java
@@ -24,7 +24,8 @@ public class ChangePasswordPage {
         androidDriver.findElement(oldPasswordInput).sendKeys(oldPassword);
     }
     public void inputNewPassword(String newPassword){
-        androidDriver.findElement(newPasswordInput).sendKeys(newPassword);
+        inputAndScroll(newPasswordInput,newPassword);
+        //androidDriver.findElement(newPasswordInput).sendKeys(newPassword);
     }
     public void inputConfirmNewPassword(String confirmNewPassword){
         androidDriver.findElement(confirmNewPasswordInput).sendKeys(confirmNewPassword);
@@ -32,5 +33,33 @@ public class ChangePasswordPage {
     public boolean seeWarningText(){
         return waitElement(warningText,15).isDisplayed();
     }
+    public static void scrollDown() {
+        AndroidElement screen = androidDriver
+                .findElement(By.id("action_bar_root"));
+        Point center =  screen.getCenter();
+        int startX = 20;
+        int startY = (int) (center.getY() * 1.5);
+        int endX = 20;
+        int endY = (int) (center.getY() * 0.5);
+        @SuppressWarnings("rawtypes")
+        TouchAction scroll = new TouchAction(androidDriver);
+        scroll.press(PointOption.point(startX, startY))
+                .moveTo(PointOption.point(endX, endY)).perform();
+    }
 
+    public static void inputAndScroll(By targetElement, String input) {
+        boolean isFound = false;
+        int counter = 0;
+
+        do {
+            try {
+                inputElement(targetElement, input);
+                isFound = true;
+            } catch (Exception e) {
+                scrollDown();
+                counter++;
+            }
+
+        } while (!isFound && counter < 5);
+    }
 }
