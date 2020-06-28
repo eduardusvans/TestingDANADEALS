4dd2871a6d70102f1b57f3596a846d66426315af
eduardusvans <eduardus.arderito@dana.id>
2020-06-28 12:24:39 +0700

    Update history locator

--- src/main/java/demo/locators/payer/history/HistoryPageLocator.java
+++ src/main/java/demo/locators/payer/history/HistoryPageLocator.java
@@ -3,7 +3,7 @@ package demo.locators.payer.history;
 import org.openqa.selenium.By;
 
 public interface HistoryPageLocator {
-    By In_Progress_Tab = By.id("");
-    By Completed_Tab = By.id("");
-
+    By In_Progress_Tab = By.xpath("//android.widget.LinearLayout[@content-desc=\"IN-PROGRESS\"]");
+    By Completed_Tab = By.xpath("//android.widget.LinearLayout[@content-desc=\"COMPLETED\"]");
+    By Filter_Date = By.id("tv_filter");
 }
