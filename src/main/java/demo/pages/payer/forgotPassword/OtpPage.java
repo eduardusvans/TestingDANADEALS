0231de8885a550747ca393c3025c7aaea535882c
Bilal El Bari <bilalelbari@gmail.com>
2020-06-30 14:11:26 +0700

    Add send otp timeout handler

--- src/main/java/demo/pages/payer/forgotPassword/OtpPage.java
+++ src/main/java/demo/pages/payer/forgotPassword/OtpPage.java
@@ -1,11 +1,22 @@
 package demo.pages.payer.forgotPassword;
+import static demo.locators.payer.forgotPassword.ForgotPasswordPageLocator.BUTTON_SEND_OTP;
 import static demo.locators.payer.forgotPassword.OtpPageLocator.*;
 import static demo.utils.ActionUtils.*;
 
 public class OtpPage {
     public boolean isOnPage() {
-
-        return waitElement(INPUT_OTP, 20).isDisplayed();
+        int count = 0;
+        do {
+            try{
+                return waitElement(INPUT_OTP, 15).isDisplayed();
+            } catch (Exception e) {
+                if(getElement(BUTTON_SEND_OTP).isDisplayed()) {
+                    tapElement(BUTTON_SEND_OTP);
+                }
+            }
+            count++;
+        } while (count < 3);
+        return false;
     }
 
     public void inputOtp(String otp) {
