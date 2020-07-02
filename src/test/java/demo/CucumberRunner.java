35eb7df4e06ff812e976b2592679aa2a0489de8e
Bilal El Bari <bilalelbari@gmail.com>
2020-07-02 18:48:57 +0700

    Add Edge Case fix

--- src/test/java/demo/CucumberRunner.java
+++ src/test/java/demo/CucumberRunner.java
@@ -9,6 +9,6 @@ import org.junit.runner.RunWith;
         stepNotifications = true,
         //for plugin check https://cucumber.io/docs/cucumber/reporting/
         plugin = {"json:build/cucumber.json", "pretty", "html:build/result"},
-        features = "src/test/resources/features", tags = {"@ForgotPassword and @EdgeCase"})
+        features = "src/test/resources/features", tags = {"@ForgotPassword"})
 public class CucumberRunner {
 }
